a131d065fd8756c2972c159bdde53a3a7b302552
4O44O4 <152752343+no-4-5o5@users.noreply.github.com>
2026-06-05 19:02:12 -0600

    Refactor canvas initialization and add resizing
    Removed mobile condition for canvas initialization and added canvas resizing functionality.

--- WHG2/canvas.js
+++ WHG2/canvas.js
@@ -1,7 +1,10 @@
-if (mobile) {
-    var canvasElement = $("<canvas oncontextmenu='return false;' id='twhgCanvas'></canvas>").get(0);
-    var canvas = canvasElement.getContext("2d");
-} else {
-    var canvasElement = $("<canvas id='twhgCanvas' width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>").get(0);
-    var canvas = canvasElement.getContext("2d");
+var canvasElement = $("<canvas oncontextmenu='return false;' id='twhgCanvas'></canvas>").get(0);
+var canvas = canvasElement.getContext("2d");
+
+function resizeCanvas() {
+    canvasElement.width = window.innerWidth;
+    canvasElement.height = window.innerHeight;
 }
+
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
